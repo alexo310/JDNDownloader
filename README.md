5d11969b2c6b623c8f08e854ed1aaa482ccf2ea8
alexo <94218972+alexo310@users.noreply.github.com>
2024-06-28 21:36:39 +0100

    ✨ remade the whole script
    added a feature to include any jdn servers you want.

--- README.md
+++ README.md
@@ -1,18 +1,28 @@
 # JDNDownloader by alexo
-**JDNDownloader** downloads files from every Just Dance Now server.
+**JDNDownloader** downloads files from every (and any) Just Dance Now server.
 ## Instructions
 To make this script work, install Python. After installing, open the folder on terminal and paste this:
 ```bash
   python -m pip install -r requirements.txt
 ```
-## Features
+## Servers
 | Server | Codename |
 | ------------- | ------------- |
 | Demo Servers (2014-2015) | demo  |
 | Old Servers (2015-2017) | uat |
 | New Servers (2018-Present) | prod |
 
+## How to add a new server
+Add a new item to the servers list in settings.json
+| Key | Type | Meaning |
+| ------------- | ------------- | ------------- |
+| cdnLink* | str | Link to the server |
+| description* | str | Self-explanatory |
+| isProdBased | bool | Tells the script if it needs an id |
+| songdbLink | str | Link to the server if it's prod based |
+
+**Note:** You'll need to know how json files work to understand this.
+
 ### Credits
-- SilencerGuy, MZommer - Giving tips on how to clean the code
 - Kubabisi - Implement linux & mac cmd into the script
 - ibratabian17 - Implement new server into the script
---
+++ src/main.py
@@ -0,0 +1,85 @@
+import json, os, requests
+
+def clear() -> None:
+    os.system('clear' if os.name == 'posix' else 'cls')
+
+class Download:
+    cdnLink: str
+    codename: str
+    serverName: str
+
+    def getSongdb(self, codename: str, url: str) -> str:
+        if not os.path.exists(f'{self.serverName}Codes.json'):
+            with requests.get(url) as songdb:
+                codes = {}
+                for song in json.loads(songdb.content):
+                    codes[song['id']] = song['base'].split('_')[-1]
+                json.dump(codes, open(f'{self.serverName}Codes.json', 'w'))
+        songdb = json.load(open(f'{self.serverName}Codes.json', encoding='utf8'))
+        return f'{codename}_{songdb[codename]}'
+
+    @staticmethod
+    def download(url: str, output: str) -> None:
+        response = requests.get(url, allow_redirects=False, stream=True)
+        match response.status_code:
+            case 403: print('access forbidden')
+            case 200 | 206:
+                os.makedirs(output, exist_ok=True)
+                with open(f'{output}/{os.path.basename(url)}', 'wb') as file:
+                    print(f'Downloading {os.path.basename(url)}')
+                    file.write(response.content)
+            case _: print(f'{os.path.basename(url)} is not available.')
+
+    def main(self, server: dict[str|bool]) -> None:
+        clear()
+        self.cdnLink: str = server['cdnLink']
+        self.download(f'{self.cdnLink}/songs/{self.codename}/assets/web/{self.codename.split("_")[0]}.ogg', f'output/{self.codename.split("_")[0]}/assets')
+        self.download(f'{self.cdnLink}/songs/{self.codename}/assets/web/{self.codename.split("_")[0].lower()}.jpg', f'output/{self.codename.split("_")[0]}/assets')
+        self.download(f'{self.cdnLink}/songs/{self.codename}/assets/web/{self.codename.split("_")[0].lower()}_small.jpg', f'output/{self.codename.split("_")[0]}/assets')
+        self.download(f'{self.cdnLink}/songs/{self.codename}/assets/web/pictos-sprite.png', f'output/{self.codename.split("_")[0]}/assets')
+        self.download(f'{self.cdnLink}/songs/{self.codename}/assets/web/pictos-sprite.css', f'output/{self.codename.split("_")[0]}/assets')
+        self.download(f'{self.cdnLink}/songs/{self.codename}/assets/web/pictos-atlas.png', f'output/{self.codename.split("_")[0]}/assets')
+        self.download(f'{self.cdnLink}/songs/{self.codename}/assets/web/pictos-atlas.json', f'output/{self.codename.split("_")[0]}/assets')
+        self.download(f'{self.cdnLink}/songs/{self.codename}/assets/app/{self.codename.split("_")[0].lower()}_cover.jpg', f'output/{self.codename.split("_")[0]}/assets')
+        self.download(f'{self.cdnLink}/songs/{self.codename}/assets/app/{self.codename.split("_")[0].lower()}_cover%402x.jpg', f'output/{self.codename.split("_")[0]}/assets')
+        self.download(f'{self.cdnLink}/songs/{self.codename}/assets/app/{self.codename.split("_")[0].lower()}_cover@2x.jpg', f'output/{self.codename.split("_")[0]}/assets')
+        self.download(f'{self.cdnLink}/songs/{self.codename}/{self.codename.split("_")[0]}.json', f'output/{self.codename.split("_")[0]}/data')
+        for i in range(4):
+            self.download(f'{self.cdnLink}/songs/{self.codename}/assets/common/coaches/{self.codename.split("_")[0].lower()}_coach_{i + 1}_big.png', f'output/{self.codename.split("_")[0]}/assets')
+            self.download(f'{self.cdnLink}/songs/{self.codename}/assets/common/coaches/{self.codename.split("_")[0].lower()}_coach_{i + 1}.png', f'output/{self.codename.split("_")[0]}/assets')
+            self.download(f'{self.cdnLink}/songs/{self.codename}/data/moves/{self.codename.split("_")[0]}_moves{i}.json', f'output/{self.codename.split("_")[0]}/data/moves')
+    
+    def prod(self, server: dict[str|bool], serverName: str):
+        self.serverName: str = serverName
+        self.codename = self.getSongdb(input('\ncodename:\n>>> '), server['songdbLink'])
+        self.main(server)
+        self.download(f'{self.cdnLink}/map_bkg/{self.codename.split("_")[0]}_map_bkg.jpg', f'output/{self.codename.split("_")[0]}/assets')
+        self.download(f'{self.cdnLink}/songs/{self.codename}/bundle.zip', f'output/{self.codename.split("_")[0]}/bundle')
+
+    def uat(self, server: dict[str|bool], serverName: str):
+        self.codename: str = input('\ncodename:\n>>> ')
+        self.main(server)
+        self.download(f'{self.cdnLink}/dist/bundle/{self.codename}.zip', f'output/{self.codename}/bundle')
+        for i in range(8):
+            self.download(f'{self.cdnLink}/dist/bundle/{self.codename}_{i+2}.zip', f'output/{self.codename}/bundle')
+
+with open('settings.json') as file:
+    settings: dict = json.load(file)
+
+def main() -> None:
+    clear()
+    print('\n- just dance now downloader by alexo\n-------------------------------------')
+    serverList: dict[function] = {}
+    for index, server in enumerate(settings['servers']):
+        serverList[str(index+1)] = Download().prod if settings['servers'][server].get('isProdBased', False) else Download().uat
+        print(f'[{index+1}] {server} - {settings["servers"][server]["description"]}')
+    print('[0] exit this script\n-------------------------------------')
+    choice: str = input('choose a number associated with the number\n>>> ')
+    serverName = list(settings['servers'].keys())[int(choice)-1]
+    if choice == '0': exit()
+    elif choice in serverList: serverList[choice](settings['servers'][serverName], serverName)
+    else: input('\nchoice is not available. press enter to return')
+    main()
+
+if __name__ == '__main__':
+    main()
