762c9ba774edaa054d1a549284fd23a5d9806210
alexo <94218972+alexo310@users.noreply.github.com>
2026-06-12 10:38:03 +0100

    Last commit

--- src/main.py
+++ src/main.py
@@ -1,93 +1,118 @@
 import json, os, requests
 
-def clear() -> None:
+def clearConsole() -> None:
     os.system('clear' if os.name == 'posix' else 'cls')
 
-class Download:
-    cdnLink: str
-    codename: str
-    serverName: str
-
-    def getSongdb(self, codename: str, url: str) -> str:
-        if not os.path.exists(f'{self.serverName}Codes.json'):
-            codes = {}
-            for song in json.loads(requests.get(url).content):
-                codes[song['id']] = song['base'].split('_')[-1]
-            json.dump(codes, open(f'{self.serverName}Codes.json', 'w'))
-        songdb = json.load(open(f'{self.serverName}Codes.json', encoding='utf8'))
-        try: return f'{codename}_{songdb[codename]}'
-        except:
-            input('song not found. returning to main menu.')
-            main()
+class Downloader:
+    def __init__(self, codename: str, **kwargs) -> None:
+        clearConsole()
+        self.codename, self.name = codename, kwargs['name']
+        print(f'\n- downloading {codename}\n' + '-' * 50)
+        try: self._main(codename, **kwargs)
+        except: input('\nthere was an error. press enter to go back.\n')
+        
+    def _main(self, codename: str, name: str, link: str, isProdBased: bool = False, **kwargs):
+        if isProdBased:
+            print('getting server links')
+            with requests.get(f'{link}/query') as query:
+                self.jdns = json.loads(query.content)['jdns']
+            print('updating song codes')
+            try:
+                with requests.get(f'{self.jdns.replace('jdns', 'api')}/v1/songs/published', stream=True) as songdb:
+                    db = self._saveDb(json.loads(songdb.content))
+            except:
+                    db = self._saveDb([])
+            song = [db[song] for song in db if song == codename]
+            if not len(song):
+                input(f'{codename} was not found. press enter to go back.\n')
+                return
+            self.base = song[0]
+        else: self.base = f'{link}/songs/{codename}'
+        self.output = f'output/{codename}/{name}'
+        self._dlFile(f'{self.base}/assets/web/{codename}.ogg', f'{self.output}/assets')
+        self._dlFile(f'{self.base}/assets/web/{codename}.mp3', f'{self.output}/assets')
+        self._dlFile(f'{self.base}/assets/web/{codename.lower()}.jpg', f'{self.output}/assets')
+        self._dlFile(f'{self.base}/assets/web/{codename.lower()}_small.jpg', f'{self.output}/assets')
+        self._dlFile(f'{self.base}/assets/web/pictos-sprite.png', f'{self.output}/assets')
+        self._dlFile(f'{self.base}/assets/web/pictos-sprite.css', f'{self.output}/assets')
+        self._dlFile(f'{self.base}/assets/app/{codename.lower()}_cover.jpg', f'{self.output}/assets')
+        try: self._dlFile(f'{self.base}/assets/app/{codename.lower()}_cover%402x.jpg', f'{self.output}/assets', True)
+        except: self._dlFile(f'{self.base}/assets/app/{codename.lower()}_cover@2x.jpg', f'{self.output}/assets')
+        self._dlFile(f'{self.base}/{codename}.json', f'{self.output}/data')
+        for i in range(4):
+            try:
+                self._dlFile(f'{self.base}/assets/common/coaches/{codename.lower()}_coach_{i+1}.png', f'{self.output}/assets', True)
+                self._dlFile(f'{self.base}/assets/common/coaches/{codename.lower()}_coach_{i+1}_big.png', f'{self.output}/assets', True)
+                self._dlFile(f'{self.base}/data/moves/{codename}_moves{i}.json', f'{self.output}/data', True)
+            except: break
+            if name != 'rio':
+                moves = json.loads(open(f'{self.output}/data/{codename}_moves{i}.json').read().split('(', 1)[1].rsplit(')', 1)[0])
+                for move in moves:
+                    url = f'{self.base}/data/classifiers{'_WIIU' if isProdBased else ''}/{move['name']}.msm'
+                    self._dlFile(url, f'{self.output}/data/classifiers', log=False)
+        self.cdn = os.path.dirname(self.base).rsplit('/', 1)[0]
+        if isProdBased: self._prodDl()
+        elif name == 'uat': self._uatDl()
+        #input(f'\n{codename} was successfully downloaded! press enter to go back.\n')
 
     @staticmethod
-    def download(url: str, output: str) -> None:
-        response = requests.get(url, allow_redirects=False, stream=True)
-        statusCode = response.status_code
-        if statusCode == 403: print('access forbidden')
-        elif statusCode in [200, 206]:
-            os.makedirs(output, exist_ok=True)
-            with open(f'{output}/{os.path.basename(url)}', 'wb') as file:
-                print(f'Downloading {os.path.basename(url)}')
-                file.write(response.content)
-        else: print(f'{os.path.basename(url)} is not available.')
-
-    def main(self, server: dict[str|bool]) -> None:
-        clear()
-        self.cdnLink: str = server['cdnLink']
-        self.download(f'{self.cdnLink}/songs/{self.codename}/assets/web/{self.codename.split("_")[0]}.ogg', f'output/{self.codename.split("_")[0]}/assets')
-        self.download(f'{self.cdnLink}/songs/{self.codename}/assets/web/{self.codename.split("_")[0]}.mp3', f'output/{self.codename.split("_")[0]}/assets')
-        self.download(f'{self.cdnLink}/songs/{self.codename}/assets/web/{self.codename.split("_")[0].lower()}.jpg', f'output/{self.codename.split("_")[0]}/assets')
-        self.download(f'{self.cdnLink}/songs/{self.codename}/assets/web/{self.codename.split("_")[0].lower()}_small.jpg', f'output/{self.codename.split("_")[0]}/assets')
-        self.download(f'{self.cdnLink}/songs/{self.codename}/assets/web/pictos-sprite.png', f'output/{self.codename.split("_")[0]}/assets')
-        self.download(f'{self.cdnLink}/songs/{self.codename}/assets/web/pictos-sprite.css', f'output/{self.codename.split("_")[0]}/assets')
-        self.download(f'{self.cdnLink}/songs/{self.codename}/assets/web/pictos-atlas.png', f'output/{self.codename.split("_")[0]}/assets')
-        self.download(f'{self.cdnLink}/songs/{self.codename}/assets/web/pictos-atlas.json', f'output/{self.codename.split("_")[0]}/assets')
-        self.download(f'{self.cdnLink}/songs/{self.codename}/assets/app/{self.codename.split("_")[0].lower()}_cover.jpg', f'output/{self.codename.split("_")[0]}/assets')
-        self.download(f'{self.cdnLink}/songs/{self.codename}/assets/app/{self.codename.split("_")[0].lower()}_cover%402x.jpg', f'output/{self.codename.split("_")[0]}/assets')
-        self.download(f'{self.cdnLink}/songs/{self.codename}/assets/app/{self.codename.split("_")[0].lower()}_cover@2x.jpg', f'output/{self.codename.split("_")[0]}/assets')
-        self.download(f'{self.cdnLink}/songs/{self.codename}/{self.codename.split("_")[0]}.json', f'output/{self.codename.split("_")[0]}/data')
-        for i in range(4):
-            self.download(f'{self.cdnLink}/songs/{self.codename}/assets/common/coaches/{self.codename.split("_")[0].lower()}_coach_{i + 1}_big.png', f'output/{self.codename.split("_")[0]}/assets')
-            self.download(f'{self.cdnLink}/songs/{self.codename}/assets/common/coaches/{self.codename.split("_")[0].lower()}_coach_{i + 1}.png', f'output/{self.codename.split("_")[0]}/assets')
-            self.download(f'{self.cdnLink}/songs/{self.codename}/data/moves/{self.codename.split("_")[0]}_moves{i}.json', f'output/{self.codename.split("_")[0]}/data/moves')
+    def _dlFile(url: str, output: str, handleExceptions: bool = False, log=True) -> None:
+        filename = os.path.basename(url)
+        if not os.path.exists(f'{output}/{filename}'):
+            with requests.get(url, allow_redirects=False, stream=True) as response:
+                if response.status_code >= 400:
+                    # really useful way i found to make for loops stop
+                    if handleExceptions: raise Exception
+                    else: print(f'{filename} is not available')
+                elif response.status_code in [200, 206]:
+                    print(f'downloading {filename}')
+                    os.makedirs(output, exist_ok=True)
+                    open(f'{output}/{filename}', 'wb').write(response.content)
+        elif log: print(f'{filename} was already downloaded.')
     
-    def prod(self, server: dict[str|bool], serverName: str):
-        self.serverName: str = serverName
-        self.codename = self.getSongdb(input('\ncodename:\n>>> '), server['songdbLink'])
-        self.main(server)
-        self.download(f'{self.cdnLink}/map_bkg/{self.codename.split("_")[0]}_map_bkg.jpg', f'output/{self.codename.split("_")[0]}/assets')
-        self.download(f'{self.cdnLink}/songs/{self.codename}/bundle.zip', f'output/{self.codename.split("_")[0]}/bundle')
+    def _saveDb(self, songdb: dict) -> dict:
+        try: newDb = json.load(open(f'{self.name}Versions.json'))
+        except: newDb = {}
+        for song in songdb:
+            if song['id'] in newDb and song['base'] == newDb[song['id']]: continue
+            newDb[song['id']] = song['base']
+        json.dump(newDb, open(f'{self.name}Versions.json', 'w'))
+        return newDb
+
+    def _prodDl(self):
+        self._dlFile(f'{self.base}/songMetadata.zip', f'{self.output}')
+        self._dlFile(f'{self.base}/assets/web/pictos-atlas.png', f'{self.output}/assets')
+        self._dlFile(f'{self.base}/assets/web/pictos-atlas.json', f'{self.output}/assets')
+        self._dlFile(f'{self.cdn}/map_bkg/{self.codename}_map_bkg.jpg', f'{self.output}/assets')
+        self._dlFile(f'{self.base}/bundle.zip', f'{self.output}/bundle')
         try:
-            with requests.get(f'{server["jdnsLink"]}/getPreviewVideo?song={self.codename.split("_")[0]}', headers={'x-platform': 'web'}) as cookies:
-                self.download(json.loads(cookies.content)['url'], f'output/{self.codename.split("_")[0]}/video')
+            with requests.get(f'{self.jdns}/getPreviewVideo?song={self.codename}', headers={'x-platform': 'web'}) as cookies:
+                self._dlFile(json.loads(cookies.content)['url'], f'{self.output}/video')
         except:
-            print(f'jdns server or video is not available.')
+            print(f'{self.codename}\'{'s' if self.codename[-1]!='s' else ''} videos are not available.')
 
-    def uat(self, server: dict[str|bool], serverName: str):
-        self.codename: str = input('\ncodename:\n>>> ')
-        self.main(server)
-        self.download(f'{self.cdnLink}/dist/bundle/{self.codename}.zip', f'output/{self.codename}/bundle')
+    def _uatDl(self):
+        self._dlFile(f'{self.cdn}/dist/bundle/{self.codename}.zip', f'{self.output}/bundle')
         for i in range(8):
-            self.download(f'{self.cdnLink}/dist/bundle/{self.codename}_{i+2}.zip', f'output/{self.codename}/bundle')
+            try: self._dlFile(f'{self.cdn}/dist/bundle/{self.codename}_{i+2}.zip', f'{self.output}/bundle', True)
+            except: break
 
-with open('settings.json') as file:
-    settings: dict = json.load(file)
 
-def main() -> None:
-    clear()
-    print('\n- just dance now downloader by alexo\n-------------------------------------')
-    serverList: dict[function] = {}
-    for index, server in enumerate(settings['servers']):
-        serverList[str(index+1)] = Download().prod if settings['servers'][server].get('isProdBased', False) else Download().uat
-        print(f'[{index+1}] {server} - {settings["servers"][server]["description"]}')
-    print('[0] exit this script\n-------------------------------------')
-    choice: str = input('choose a number associated with the number\n>>> ')
-    serverName = list(settings['servers'].keys())[int(choice)-1]
-    if choice == '0': exit()
-    elif choice in serverList: serverList[choice](settings['servers'][serverName], serverName)
-    else: input('\nchoice is not available. press enter to return')
+def main():
+    clearConsole()
+    print(f'\n- j___ d____ now downloader by a_______\n' + '-' * 50)
+    for ind, server in enumerate(settings['servers']):
+        print(f'[{ind+1}] {server["name"]} - {server['description']}')
+    print('[0] exit this script\n' + '-' * 50)
+    if True: #try:
+        choice = int(input('choose a number associated with an option\n>>> '))
+        if choice > 0 and choice <= len(settings['servers']):
+            MapName = input('\ninsert the codename here:\n>>> ')
+            Downloader(MapName, **settings['servers'][choice-1])
+        elif choice == 0: exit()
+    #except: input('that choice isn\'t available')
     main()
 
 if __name__ == '__main__':
+    settings = json.load(open('settings.json'))
     main()
